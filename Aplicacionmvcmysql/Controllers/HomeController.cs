4bde78528da4f6a72e708a5f0fc5ddbd7093b8c0
gtapiero <giovanny.tapiero@pi.edu.co>
2025-03-28 14:55:35 -0500

    Agregar archivos de proyecto.

---
+++ Aplicacionmvcmysql/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Aplicacionmvcmysql.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+
+namespace Aplicacionmvcmysql.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //connection string
+      //  private readonly string connectionString = "server=localhost;database=mi_base;user=mi_usuario;password=mi_contraseña;";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+         /*   DataTable dataTable = new DataTable();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("SELECT * FROM productos", connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                adapter.Fill(dataTable);
+            }*/
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
